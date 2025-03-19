0ebc8d0a06e9b03dd490bc1d1a8ded3085e43811
Arpan Dhama <arpandhama2001@gmail.com>
2025-03-20 01:15:07 +0530

    Started Booking implementation

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { InfoController } = require("../../controllers");
+const bookingRoutes = require("./booking");
 
 router.get("/info", InfoController.info);
+router.use("/bookings", bookingRoutes);
 
 module.exports = router;
