6250f3d8ebd4583eed0a071ee660743c20db9c63
Arpan Dhama <arpandhama2001@gmail.com>
2025-08-05 10:04:56 +0530

    Added Dockerfile

--- src/config/queue-config.js
+++ src/config/queue-config.js
@@ -3,28 +3,26 @@ const amqplib = require("amqplib");
 let channel, connection;
 
 async function connectQueue() {
-    try {
-        connection = await amqplib.connect("amqp://localhost");
-        channel = await connection.createChannel();
+  try {
+    // connection = await amqplib.connect("amqp://localhost");
+    connection = await amqplib.connect("amqp://rabbitmq");
+    channel = await connection.createChannel();
 
-        await channel.assertQueue("noti-queue");
-    } catch (error) {
-        console.log(error);
-    }
+    await channel.assertQueue("noti-queue");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function sendData(data) {
-    try {
-        await channel.sendToQueue(
-            "noti-queue",
-            Buffer.from(JSON.stringify(data))
-        );
-    } catch (error) {
-        console.log("queue error", error);
-    }
+  try {
+    await channel.sendToQueue("noti-queue", Buffer.from(JSON.stringify(data)));
+  } catch (error) {
+    console.log("queue error", error);
+  }
 }
 
 module.exports = {
-    connectQueue,
-    sendData,
+  connectQueue,
+  sendData,
 };
